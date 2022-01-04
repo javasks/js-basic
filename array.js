211e76b21e10976dce7bc2bf295b938a81de87ea
javasks <ankurmall89@gmail.com>
2022-01-04 13:47:31 +0530

    initials

---
+++ array.js
@@ -0,0 +1,50 @@
+
+ const sampleArray=["Sandeep",12345,true,"hjbhdfhjbfv"]
+
+sampleArray[3]="singh"
+sampleArray.push("ramesh")
+console.log(sampleArray)
+sampleArray.pop()
+console.log(sampleArray)
+console.log(sampleArray.length)
+
+
+
+//let item="test"
+ /* const collection = ["ABC",123,true,1]
+collection[2]='Sandeep'
+console.log(collection)
+collection[collection.length]='hello'
+collection[10]="helloHowareyou"
+console.log(collection)  */
+//collection[collection.length]='hello'
+//collection[8]="dude"
+//console.log(collection[6])
+//console.log(collection)
+//console.log(collection.length)
+//console.log(collection[1])
+
+//==================================================
+/*  let anotherExample=["San", "Kumar", "Singh"]
+anotherExample.push("hello","cool", "dude") */
+//anotherExample.unshift('hey')
+//anotherExample.pop()
+//anotherExample.shift()
+//anotherExample.pop()
+//anotherExample.sort()
+ //console.log(anotherExample) 
+
+ /* let firstLargest=anotherExample.find(function(test){
+    if (test.length>=4){
+    return test
+    }
+    })
+    console.log('firstLargest:', firstLargest)  
+    //===============
+    
+     var number=[1,2,3,4,5];
+    number.push(6);
+    number.unshift(7)
+    number.pop()
+    number.shift()
+    console.log(number)  */
